e6b4194456f2f596e2820c888ecc52d0094b70cb
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-06-13 13:29:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'
+export const App = () => {
+  return(
+    <div class="bg-cyan-400 p-4 shadow-xl	main m-auto my-10 border-3 rounded-2xl hover:shadow-2xl cursor-pointer">
+        <h1 class="text-center text-xl text-rose-800 font-bold mb-5">Tailwind</h1>
+        <p class="text-justify leading-loose p-4	">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus eos sed voluptatibus molestias ipsa officia dicta illum similique pariatur commodi, soluta possimus ullam mollitia consequatur eius neque corporis distinctio.</p>
     </div>
   );
 }
-
-export default App;
